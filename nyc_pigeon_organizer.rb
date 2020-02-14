fde9bb3c85b7b00799d5288c03fc9b9e6d53f151
Samantha Ponce <samanthaponce5@gmail.com>
2020-02-14 16:51:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,6 @@
-def nyc_pigeon_organizer(data)
-  new_hash = {}
-
-  data.each do |keys, values|
-    values.each do |k, v|
-      v.each do |name|
-        new_hash[name] ||= {}
-        new_hash[name][keys] ||= []
-        new_hash[name][keys].push(k.to_s)
-      end
-    end 
+def nyc_pigeon_organizer(pigeon_data)
+  new = {}
+  pigeon_data.each do |key, value|
+    value.collect
   end
-
- new_hash
 end
